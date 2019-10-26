06e13f9cd04d0149aa7f323e5e327072c4dd3fed
Sultanyaron <sultanyaron@gmail.com>
2019-10-26 14:13:18 +0300

    Initializing the store and middlewares

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './containers/Home/Home';
+
+const App = props => {
+
+
+    let routes = (
+        <Switch>
+            <Route path='/favorites' />
+            <Route path='/' redner={() =>  <Home {...props} />} />
+            <Redirect to='/' />
+        </Switch>
+    ); 
+
+    return (
+        <div className="App">
+            123
+        </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { compose, combineReducers, createStore, applyMiddleware } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk';
+import currentCityReducer from './store/reducers/currentCityReducer';
+import favoriteReducer from './store/reducers/favoritesReducer';
+
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers({
+    currentCity: currentCityReducer,
+    favorites : favoriteReducer
+});
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk, sagaMiddleware)
+));
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
+
 serviceWorker.unregister();
