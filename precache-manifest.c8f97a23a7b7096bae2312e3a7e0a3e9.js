06e2148092f1d81108f17a136f011518080e3db0
Sultanyaron <sultanyaron@gmail.com>
2019-10-26 17:56:28 +0300

    Updates

---
+++ precache-manifest.c8f97a23a7b7096bae2312e3a7e0a3e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e65e775cc8e2aa94b06895c1b591e2",
+    "url": "/Weather-App-v2/index.html"
+  },
+  {
+    "revision": "ac628faad0f8ebeeab52",
+    "url": "/Weather-App-v2/static/css/main.14864c40.chunk.css"
+  },
+  {
+    "revision": "fb8e45ef1ff27f4a8ff4",
+    "url": "/Weather-App-v2/static/js/2.c2164012.chunk.js"
+  },
+  {
+    "revision": "298bd83c392f471ff6c5",
+    "url": "/Weather-App-v2/static/js/3.e24a648a.chunk.js"
+  },
+  {
+    "revision": "ac628faad0f8ebeeab52",
+    "url": "/Weather-App-v2/static/js/main.53d5e780.chunk.js"
+  },
+  {
+    "revision": "f58c021b1862d44ef776",
+    "url": "/Weather-App-v2/static/js/runtime-main.cdc845eb.js"
+  },
+  {
+    "revision": "802510afdc1a113e90e8894ac95142db",
+    "url": "/Weather-App-v2/static/media/celsius.802510af.svg"
+  },
+  {
+    "revision": "0a3403d123891c2901487932eed25a3d",
+    "url": "/Weather-App-v2/static/media/fahrenheit.0a3403d1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Weather-App-v2/precache-manifest.a03a2ef6da280cacc2697d2e134577ec.js"
+  "/Weather-App-v2/precache-manifest.c8f97a23a7b7096bae2312e3a7e0a3e9.js"
 );
 
 self.addEventListener('message', (event) => {
